5ed9cde2581555e65ea1d4a2f9e02cf354c42cf9
EDY <18519733556@163.com>
2026-01-21 14:17:02 +0800

    gateway

--- Project/YB/hos-ms-gateway/src/main/java/com/hospital/gateway/config/DynamicRouteConfig.java
+++ Project/YB/hos-ms-gateway/src/main/java/com/hospital/gateway/config/DynamicRouteConfig.java
@@ -5,6 +5,7 @@ import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.listener.Listener;
 import lombok.extern.slf4j.Slf4j;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.config.GatewayProperties;
